ea68ca8a073312d2d223d8ddfaa3e59f41f728d5
Grace Mwai <gracejmwai@gmail.com>
2026-07-08 08:07:38 +0300

    Added responsiveness

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import styles from './page.module.css'; 
 
-
 export default function Home() {
   return (
     <main className={styles.mainContainer}>
